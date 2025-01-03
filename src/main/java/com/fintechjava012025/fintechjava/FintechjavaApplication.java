4eea184f6d5b06a3c515195f92c1d3ba065055d0
marce1084 <marcelo.grra@gmail.com>
2025-01-03 07:55:38 -0300

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/fintechjava012025/fintechjava/FintechjavaApplication.java
@@ -0,0 +1,13 @@
+package com.fintechjava012025.fintechjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fintechjava012025/fintechjava/FintechjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintechjava012025.fintechjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
