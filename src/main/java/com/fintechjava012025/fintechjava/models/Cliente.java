3dce4df5a91169f35840eeefddffa40f5b688eaf
marce1084 <marcelo.grra@gmail.com>
2025-01-15 00:43:01 -0300

    Se ejecuto mvn clean install

--- src/main/java/com/fintechjava012025/fintechjava/models/Cliente.java
+++ src/main/java/com/fintechjava012025/fintechjava/models/Cliente.java
@@ -1,34 +1,33 @@
 package com.fintechjava012025.fintechjava.models;
 
-import com.sun.istack.internal.NotNull;
-import org.apache.logging.log4j.message.Message;
-import org.hibernate.annotations.BatchSize;
-import jakarta.validation.constraints.Email;
-import org.hibernate.annotations.processing.Pattern;
+//import com.sun.istack.internal.NotNull;
+//import jakarta.validation.constraints.Size;
+//import org.apache.logging.log4j.message.Message;
+//import org.hibernate.annotations.BatchSize;
+//import jakarta.validation.constraints.Email;
+//import org.hibernate.annotations.processing.Pattern;
 
 public class Cliente {
     //Atributos
-    @NotNull(String Message = "El DNI no puede estar vacío")
+    //@NotNull //(String Message = "El DNI no puede estar vacío");
     private Long DNI;
 
-    @NotNull(String Message = "El nombre no puede estar vacío")
-    @BatchSize(size = 0)
-    int min(3), int max(100), String Message = "El nombre debe tener entre 3 y 100 caracteres"
+    //@NotNull //(String Message = "El nombre no puede estar vacío");
+    //@BatchSize(size = 0)
+    //int min(3), int max(100), String Message = "El nombre debe tener entre 3 y 100 caracteres"
     private String nombre;
 
-    @BatchSize(size = 0)
-    int min(3), int max(100), String Message = "El apellido debe tener entre 3 y 100 caracteres"
+    //@Size (int min(3), int max(100)); //, String Message = "El apellido debe tener entre 3 y 100 caracteres");
     private String apellido;
 
-    @NotNull(String Message = "El correo no puede estar vacío")
-    @Email (String Message = "Debe proporcionar un mail válido")
+    //@NotNull //(String Message = "El correo no puede estar vacío");
+    //@Email // (String Message = "Debe proporcionar un mail válido");
     private String mail;
 
-    @NotNull(String Message = "La contraseña no puede estar vacía")
-    @BatchSize(size = 0)
-    int min = 8, int max = 20, String message = "La contraseña debe tener entre 8 y 20 caracteres"
-    @Pattern(String regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$",
-    String message = "La contraseña debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial")
+    //@NotNull //(String Message = "La contraseña no puede estar vacía");
+    //@Size int min = 8, int max = 20; //, String message = "La contraseña debe tener entre 8 y 20 caracteres");
+    //@Pattern (String regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$",
+    // String message = "La contraseña debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial");
     private String contrasenia;
 
     //Constructur vacío (necesario para jpa)
