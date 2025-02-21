ce9dfa0e41474f66c4807c91dd288cb18a0be25a
marce1084 <marcelo.grra@gmail.com>
2025-02-21 17:23:10 -0300

    se creo la clase Transaccion

---
+++ src/main/java/com/fintechjava012025/fintechjava/models/Transaccion.java
@@ -0,0 +1,69 @@
+package com.fintechjava012025.fintechjava.models;
+
+import com.sun.istack.internal.NotNull;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import java.util.Date;
+
+public class Transaccion {
+    @Id
+    private Long id;
+
+    @NotNull
+    private Double cantidad;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotNull
+    private Date fecha;
+
+    @NotNull
+    private String tipo;
+
+    @NotNull
+    private String motivo;
+
+    //Contructor, setters y getters
+    public Transaccion() {}
+
+    public Transaccion(Long id, Double cantidad, Date fecha, String tipo, String motivo) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.motivo = motivo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Double getCantidad() {
+        return cantidad;
+    }
+    public void setCantidad(Double cantidad) {
+        this.cantidad = cantidad;
+    }
+    public Date getFecha() {
+        return fecha;
+    }
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public String getMotivo() {
+        return motivo;
+    }
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+
+}
