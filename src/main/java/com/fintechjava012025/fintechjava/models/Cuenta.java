8b2bb86a3e486efb51e216c6ec1b153c6a7cc900
marce1084 <marcelo.grra@gmail.com>
2025-03-20 07:45:44 -0300

    Se mejoró la clase Cuenta

--- src/main/java/com/fintechjava012025/fintechjava/models/Cuenta.java
+++ src/main/java/com/fintechjava012025/fintechjava/models/Cuenta.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cuenta {
     @Id
@@ -21,8 +23,7 @@ public class Cuenta {
     //La relación @OneToMany debe ser una lista de transacciones, no una sola transacción.
     // Es recomendable usar mappedBy para indicar que la relación es bidireccional.
     @OneToMany(mappedBy = "cuenta",cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "movimiento_id", nullable = false)
-    private Transaccion transaccion;
+    private List<Transaccion> transacciones = new ArrayList<>();
 
     //Constructor, getters y setters
     public Cuenta() {}
@@ -71,30 +72,49 @@ public class Cuenta {
         this.transaccion = transaccion;
     }
 
-    //Metodo para depositar dinero
+    //Metodo depositar
     public void depositar(Double monto, String motivo) {
-        if (monto != null || monto > 0) {
+        if (monto != null && monto > 0) {
             this.saldo += monto; // // "this.saldo" se refiere al atributo de la clase
+            //Registrar la transacción
+            Transaccion transaccion = new Transaccion(
+                    null, //El id se genera automáticamente
+                    monto,
+                    LocalDateTime.now(), // Fecha y hora de la transacción
+                    "Depósito", //Tipo de transacción
+                    motivo, // Descripción o motivo de la transacción
+                    this // Cuenta asociada a la transacción, hace referencia a la instancia actual de la clase Cuenta
+            );
+            this.transacciones.add(transaccion);
         } else {
             throw new IllegalArgumentException("El monto a depositar deber ser mayor que 0.");
         }
-        //Registrar la transacción
-        Transaccion transaccion = new Transaccion(
-                null, //El id se genera automáticamente
-                monto,
-                LocalDateTime.now(), // Fecha y hora de la transacción
-                "Retiro", //Tipo de transacción
-                motivo, // Descripción o motivo de la transacción
-                this // Cuenta asociada a la transacción, hace referencia a la instancia actual de la clase Cuenta
-        );
     }
 
+    //Metodo retirar
+    public void retirar(Double monto, String motivo) {
+        if (monto !=0 && monto > 0 && monto <= this.saldo) {
+            this.saldo -= monto;
+            //Registrar la transacción
+            Transaccion transaccion = new Transaccion(
+                    null,
+                    monto,
+                    LocalDateTime.now(),
+                    "Retiro",
+                    motivo,
+                    this
+            );
+            this.transacciones.add(transaccion);
+        } else {
+            throw new IllegalArgumentException("Saldo insuficiente o monto incorrecto.");
+        }
+    }
 
-    public Double calcularSaldo() {
+    /*public Double calcularSaldo() {
         return saldo;
     }
     public Double calcularSaldoInicial() {
         return saldoInicial;
-    }
+    }*/
 
 }
